2a47e49ea4254fb83a724e571dbf8b99f2b9db6b
hpctav <hpctav@gmail.com>
2026-02-19 14:21:36 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -19,3 +19,8 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
+
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+}
