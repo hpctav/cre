c6fc08d5ad2d5e2f74d38f9116e89e6c8dfaa7d0
hpctav <hpctav@gmail.com>
2026-02-19 11:20:07 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,21 @@
 const CACHE_NAME = 'editor-v1';
 const ASSETS = [
-  'index.html',
-  'style.css',
-  'app.js',
-  'manifest.json',
-  'https://fonts.googleapis.com'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.png'
 ];
 
-// Cài đặt và lưu trữ các file vào bộ nhớ máy
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Lấy dữ liệu từ bộ nhớ đệm khi không có mạng
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
